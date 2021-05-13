cf9dc16a221c57efe6f2c7b2f52a207f5b9fd28b
GooseDimkin <lebedevdmitry7660882@gmail.com>
2021-05-13 18:26:06 +0300

    Bug fix

--- src/components/AddContactPage/AddContactPage.jsx
+++ src/components/AddContactPage/AddContactPage.jsx
@@ -42,8 +42,8 @@ function AddContactPage(props) {
     contacts_array.push(newContact);
 
     contacts_array.sort(function(a, b){
-      if(a.name.toLowerCase() < b.name.toLowerCase()) { return -1; }
-      if(a.name.toLowerCase() > b.name.toLowerCase()) { return 1; }
+      if(a.name < b.name) { return -1; }
+      if(a.name > b.name) { return 1; }
       return 0;
     })
 
