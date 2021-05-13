4779f259e5d7b64f81a40b6cd0fe7fa27655199d
GooseDimkin <lebedevdmitry7660882@gmail.com>
2021-05-13 16:24:35 +0300

    Bug fix

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import style from './App.module.css';
 import {Redirect} from 'react-router-dom';
 import {connect} from 'react-redux'
 import AddContactPage from './components/AddContactPage/AddContactPage';
+import EditContactPage from './components/EditContactPage/EditContactPage';
 import { useState } from 'react';
 import Contact from './components/Contact/Contact';
 
@@ -14,14 +15,45 @@ function App(props) {
     window.location.reload();
   }
 
-  const [modalActive, setModalActive] = useState(false);
+  const [modalAddContactActive, setModalAddContactActive] = useState(false);
+  const [modalEditContactActive, setModalEditContactActive] = useState(false);
 
   let contacts = JSON.parse(localStorage.getItem('contacts'))
-  debugger;
 
+  const download = function(data) {
+    const blob = new Blob([data], {type: 'text/csv'});
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.setAttribute('hidden', '');
+    a.setAttribute('href', url);
+    a.setAttribute('download', 'download.csv');
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  }
+
+  let csvData;
   let contactsItem;
   if(contacts) {
-    contactsItem = contacts.map(c => <Contact name={c.name} key={c} avatar={c.avatar} />)
+    let objectToCsv = function(contacts) {
+      const csvRows = [];
+      const headers = Object.keys(contacts[0]);
+      csvRows.push(headers.join(','));
+  
+      for(const row of contacts) {
+        const values = headers.map(header => {
+          const escaped = (''+row[header]).replace(/"/g, '\\"');
+          return `"${escaped}"`;
+        });
+        csvRows.push(values.join(','));
+      }
+      return csvRows.join('\n');
+    }
+  
+    csvData = objectToCsv(contacts);
+
+    let id = -1;
+    contactsItem = contacts.map(c => <Contact id={id+=1} setModalEditContactActive={() => setModalEditContactActive} name={c.name} phone={c.phone} key={c} avatar={c.avatar} />)
   }
 
   return (
@@ -32,8 +64,10 @@ function App(props) {
       {!localStorage.getItem('isAuth') && <Redirect to='/login'/>}
       <div className={style.user_name}>Hello, {localStorage.getItem('userName')}</div>
       <button className={style.logout_button} onClick={logout}>Logout</button>
-      <button className={style.new_contact_button} onClick={() => setModalActive(true)} >New Contact</button>
-      <AddContactPage active={modalActive} setActive={setModalActive} />
+      <button className={style.new_contact_button} onClick={() => setModalAddContactActive(true)} >New Contact</button>
+      <button className={style.csv_contact_button} onClick={() => download(csvData)} >Download CSV</button>
+      <AddContactPage modalAddContactActive={modalAddContactActive} setModalAddContactActive={setModalAddContactActive} />
+      <EditContactPage modalEditContactActive={modalEditContactActive} setModalEditContactActive={setModalEditContactActive} />
       <div className={style.contacts}>
         {contactsItem}
       </div>
--- src/components/EditContactPage/EditContactPage.jsx
+++ src/components/EditContactPage/EditContactPage.jsx
@@ -1,9 +1,9 @@
-import style from './AddContactPage.module.css';
+import style from './EditContactPage.module.css';
 import {connect} from 'react-redux';
 import {getContactNameAC, getContactPhoneAC, addContactAC, clearFieldsAC} from './../../redux/reducers/contactsReducer';
 import React from 'react';
 
-function AddContactPage(props) {
+function EditContactPage(props) {
 
   let nameRef = React.createRef();
   let phoneRef = React.createRef();
@@ -28,39 +28,37 @@ function AddContactPage(props) {
       return
     }
 
-    let seed = 1;
-    seed += Math.floor(Math.random() * 99);
+    let contacts = JSON.parse(localStorage.getItem('contacts'));
+    let key = localStorage.getItem('id');
 
     let newContact = {
       name: props.currentContactName,
       phone: props.currentContactPhone,
-      avatar: 'https://picsum.photos/seed/' + seed + '/200/200'
+      avatar: contacts[key].avatar
     }
-    
-    let contacts_array = [];
-    contacts_array = JSON.parse(localStorage.getItem('contacts')) || [];
-    contacts_array.push(newContact);
 
-    contacts_array.sort(function(a, b){
-      if(a.name < b.name) { return -1; }
-      if(a.name > b.name) { return 1; }
-      return 0;
+    contacts[key] = newContact;
+    
+    contacts.sort(function(a, b){
+        if(a.name < b.name) { return -1; }
+        if(a.name > b.name) { return 1; }
+        return 0;
     })
 
-    localStorage.setItem('contacts', JSON.stringify(contacts_array));
-
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+    
     props.clearFieldsAC();
-    return props.setModalAddContactActive(false);
+    return props.setModalEditContactActive(false);
   }
 
   return (
-    <div className={props.modalAddContactActive ? `${style.modal} ${style.active}` : style.modal} onClick={() => props.setModalAddContactActive(false)}>
-        <div className={props.modalAddContactActive ? `${style.modal__content} ${style.active}` : style.modal__content} onClick={e => e.stopPropagation()}>
-            <div className={style.label}>New contact</div>
+    <div className={props.modalEditContactActive ? `${style.modal} ${style.active}` : style.modal} onClick={() => props.setModalEditContactActive(false)}>
+        <div className={props.modalEditContactActive ? `${style.modal__content} ${style.active}` : style.modal__content} onClick={e => e.stopPropagation()}>
+            <div className={style.label}>Edit contact</div>
             <div className={style.content_center}><input className={style.input} ref={nameRef} value={props.currentContactName} onChange={getContactName} placeholder='Contact Name' type='text' /></div>
-            <div className={style.content_center}><input type='number' className={style.input} ref={phoneRef} value={props.currentContactPhone} onChange={getContactPhone} placeholder='Contact Phone' /></div>
+            <div className={style.content_center}><input type='number' className={style.input} ref={phoneRef} value={props.currentContactPhone} onChange={getContactPhone} placeholder='Contact Phone'/></div>
             <div className={style.content_center}><button className={style.save_button} onClick={addContact}>Save</button></div>
-            <div className={style.content_center}><button className={style.close_button} onClick={() => props.setModalAddContactActive(false)}>Close</button></div>
+            <div className={style.content_center}><button className={style.close_button} onClick={() => props.setModalEditContactActive(false)}>Close</button></div>
         </div>
     </div>
   );
@@ -73,6 +71,6 @@ const mapStateToProps = (state) => {
   }
 }
 
-let AddContactPageContainer = connect(mapStateToProps, {getContactNameAC, getContactPhoneAC, addContactAC, clearFieldsAC})(AddContactPage);
+let EditContactPageContainer = connect(mapStateToProps, {getContactNameAC, getContactPhoneAC, addContactAC, clearFieldsAC})(EditContactPage);
 
-export default AddContactPageContainer;
+export default EditContactPageContainer;
